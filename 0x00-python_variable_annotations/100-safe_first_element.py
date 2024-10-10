976126a5c8b8fcfa26012e0fc6da78d5f783b90f
LindaniMags <lindanimags@gmail.com>
2024-10-10 21:19:40 +0000

    advanced

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Augmented code
+"""
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Duck typing - first element of a sequence.
+    """
+    
+    if lst:
+        return lst[0]
+    else:
+        return None
